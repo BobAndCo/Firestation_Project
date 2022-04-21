133dbc6a98f186f2d110f6733f552b28ff619631
kylieW1122 <monkeykylie1122@gmail.com>
2022-04-21 10:21:46 -0400

    Added fileReader and bufferedReader
    Missing code in while-loop to readline and store them into the array.

--- Src/Graph.java
+++ Src/Graph.java
@@ -1,10 +1,32 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+/**[Graph.java]
+  * This is Algorithms Assignment - Fire Stations Planner 
+  * @author Khrush, Kylie, Alon - ICS4UE
+  * @version1.0, April 21, 2022
+  */
+
 public class Graph {
-
-    Graph(){
-
+    boolean[][] map;
+    static FileReader fileReader;
+    static String fileName = "communityInput_";
+    static BufferedReader input;
+    
+    Graph(int type){
+        try{
+        fileReader = new FileReader(fileName + type + ".txt");
+        input = new BufferedReader(fileReader);
+        
+        while(input.ready()){
+           // input.readLine();
+        }input.close();
+        
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
-    public static getMap(File cityMap){
+    public static void getMap(){
 
     }
 
