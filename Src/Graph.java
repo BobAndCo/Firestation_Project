e8fcaadcdde4b2da77461a6c116132c0082988f7
kylieW1122 <monkeykylie1122@gmail.com>
2022-04-21 23:19:06 -0400

    Successful taken input from txt, created an adjacency list (two ArrayList) to store the input
    I did some tricks on getting the input and added a method for printing the arrayList

--- Src/Graph.java
+++ Src/Graph.java
@@ -1,33 +1,60 @@
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
 /**[Graph.java]
   * This is Algorithms Assignment - Fire Stations Planner 
+  * This is a class representing the community(graph)
   * @author Khrush, Kylie, Alon - ICS4UE
-  * @version1.0, April 21, 2022
+  * @version1.1, April 21, 2022
   */
 
 public class Graph {
     boolean[][] map;
+    ArrayList<ArrayList<Integer>> arrayList;
     static FileReader fileReader;
     static String fileName = "communityInput_";
     static BufferedReader input;
     
     Graph(int type){
+        arrayList = new ArrayList<ArrayList<Integer>> ();
+        
+        /***********read input from text file************/
         try{
-        fileReader = new FileReader(fileName + type + ".txt");
-        input = new BufferedReader(fileReader);
-        
-        while(input.ready()){
-           // input.readLine();
-        }input.close();
-        
+            fileReader = new FileReader("TestCases/" + fileName + type + ".txt");
+            input = new BufferedReader(fileReader);
+            String str;
+            while(input.ready()){
+                str = input.readLine();            //format as: 1 - 0, 2, 5
+                str = str.substring(4);            // only take 0, 2, 5 for operations 
+                
+                String[] connectionStr = str.split(" *, *");    // sample: [0, 2, 5]
+                Integer[] connectionIntArray = new Integer[connectionStr.length];
+                for(int node=0; node<connectionStr.length; node++){ // change it into int array
+                    connectionIntArray[node] = Integer.parseInt(connectionStr[node]);
+                }
+                List<Integer> newList = Arrays.asList(connectionIntArray);
+                arrayList.add(new ArrayList<Integer>(newList));
+            }input.close();
+            
         }catch(Exception e){
             e.printStackTrace();
         }
+        
+        
+        printArrayList();
     }
-
+    
+    public void printArrayList(){
+        for(int i=0; i<this.arrayList.size(); i++){
+            System.out.println(this.arrayList.get(i).toString());
+        }
+    }
+    
     public static void getMap(){
-
+        
     }
-
+    
 }
